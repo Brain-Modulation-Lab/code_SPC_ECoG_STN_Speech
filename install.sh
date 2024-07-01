7e0aa7ae69f0d763598fe363212e324edbae9cfb
Matteo Vissani <matteo.vissani92@gmail.com>
2024-07-01 16:06:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl https://zenodo.org/api/records/12609223/draft/files/paper_SPC_ECoG_STN_Speech.zip/content
+curl https://zenodo.org/records/12610957/files/SPC_ECoG_STN_Speech.zip?download=1
 git clone https://github.com/Brain-Modulation-Lab/code_SPC_ECoG_STN_Speech.git
 
 cp SPC_ECoG_STN_Speech/demos/intracranial-data-examples/intracranial_data.mat code_SPC_ECoG_STN_Speech/demos/intracranial-data-examples/
